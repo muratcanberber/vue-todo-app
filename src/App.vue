01b235d9adc721d4bb63fccc12ba71453481f88c
Murat Can Berber <muratcanberber@gmail.com>
2023-11-06 22:53:31 +0300

    add and remove functionality added

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,34 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
+  <nav class="navbar has-background-dark has-text-white">
+    <div class="navbar-brand has-background-success px-6">
+      <a class="navbar-item has-text-white is-size-4" href="/"> TODO </a>
+    </div>
+    <RouterLink to="/" class="navbar-item navbar-end" id="menuItem"
+      >Home</RouterLink
+    >
+    <RouterLink to="/about" class="navbar-item" id="menuItem">About</RouterLink>
   </nav>
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+@keyframes example {
+  from {
+    background-color: black;
+  }
+  to {
+    background-color: white;
+  }
+}
+
+#menuItem {
+  color: white;
+}
+#menuItem:hover {
+  color: black;
+  animation-name: example;
+  animation-duration: 0.5s;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,50 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { v4 as uuidv4 } from "uuid";
+const items = ref([
+  {
+    id: uuidv4(),
+    name: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum dolore accusamus dignissimos soluta veniam voluptatum perferendis velit quos, autem ipsam.a",
+  },
+]);
 
-<template>test 3</template>
+let newTodo = ref("");
 
-<style scoped></style>
+function addNewTodo() {
+  items.value.push({
+    id: uuidv4(),
+    name: newTodo.value,
+  });
+}
+
+function removeTodo(id) {
+  items.value = items.value.filter((item) => item.id !== id);
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="box mt-5 columns">
+      <input
+        class="input is-large"
+        type="text"
+        placeholder="Type a new TODO"
+        v-model="newTodo"
+      />
+      <button class="button is-primary is-large ml-4" @click="addNewTodo">
+        ADD
+      </button>
+    </div>
+  </div>
+  <section class="container">
+    <div class="box mt-5 columns" v-for="(item, key) in items" :key="key + 1">
+      <div class="column is-italic is-bold">#{{ key + 1 }}</div>
+      <div class="column is-two-thirds">{{ item.name }}</div>
+      <div class="column has-text-right">
+        <button class="button is-danger" @click="removeTodo(item.id)">X</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style></style>
